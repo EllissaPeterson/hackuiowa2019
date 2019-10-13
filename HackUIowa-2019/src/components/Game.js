5990dddb8a191b23807c7c5bb4625ad82ec21122
EllissaPeterson <ellissa@iastate.edu>
2019-10-12 22:36:24 -0500

    stuff

---
+++ HackUIowa-2019/src/components/Game.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button, Form, Label, FormGroup, Input, Redirect } from 'reactstrap';
+
+export default class Game extends React.Component {
+
+    render() {
+        return (
+            <header className="App-header">
+                Game
+            </header>
+        )
+    }
+}
